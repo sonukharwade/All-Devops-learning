a0005c4a29417e9c1781c6ba4e16242f9ca340b1
kharw <kharwadesonu95@gmail.com>
2025-02-19 20:55:05 +0530

    commiting catalouge file

--- shipping.sh
+++ shipping.sh
@@ -2,7 +2,7 @@ dnf install maven -y
 useradd roboshop
 
 cp shipping.service /etc/systemd/system/shipping.service
-
+rm -rf /app
 mkdir /app
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip
 cd /app
